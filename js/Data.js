e4c86d39e9c60fb48d32a1897e26a3efc625e090
Hyakjbw <123542925+Hyakjbw@users.noreply.github.com>
2026-06-22 12:30:45 +0700

    Add files via upload

---
+++ js/Data.js
@@ -0,0 +1,74 @@
+const tremcData = {
+  "topbal": [
+    {
+      "player_name": "AceMindx",
+      "money": 6102
+    },
+    {
+      "player_name": "AceMindx2noOP",
+      "money": 1000
+    }
+  ],
+  "topdonnet": [
+    {
+      "player_name": "AceMindx2noOP",
+      "donnet": 0
+    },
+    {
+      "player_name": "AceMindx",
+      "donnet": 0
+    }
+  ],
+  "topplaytime": [
+    {
+      "player_name": "AceMindx",
+      "timePlayer": 3
+    },
+    {
+      "player_name": "AceMindx2noOP",
+      "timePlayer": 0
+    }
+  ],
+  "toptier": [
+    {
+      "player_name": "AceMindx",
+      "elo": 1000,
+      "rank": "C"
+    },
+    {
+      "player_name": "AceMindx2noOP",
+      "elo": 100,
+      "rank": "C"
+    }
+  ],
+  "player_stats": [
+    {
+      "uuid": "7da75bc6-f59d-3d47-afcb-5831890a47f8",
+      "player_name": "AceMindx2noOP",
+      "elo": 100,
+      "rank": "C",
+      "money": 1000,
+      "points": 0,
+      "sharp": 0,
+      "server_rank": 0,
+      "kill": 0,
+      "timePlayer": 0,
+      "donnet": 0,
+      "death": 0
+    },
+    {
+      "uuid": "8c5c5aa5-e118-3394-b322-0f1d04d85f33",
+      "player_name": "AceMindx",
+      "elo": 1000,
+      "rank": "C",
+      "money": 6102,
+      "points": 99,
+      "sharp": 99,
+      "server_rank": 0,
+      "kill": 0,
+      "timePlayer": 3,
+      "donnet": 0,
+      "death": 32
+    }
+  ]
+}
---
+++ js/changelog.js
@@ -0,0 +1,67 @@
+export const changelogData = [
+    {
+        "season": "2",
+        "date": "2026-06-7 - 2026-06-8",
+        "title": "Dự kiến ra mắt Season2 - Big Update",
+        "changes": [
+            "update độ rộng world ±25,000 blocks",
+            "Nâng cấp cấu hình và tối ưu lại Server",
+            "Nâng cấp, enchant bằng tiền hay vì kinh nghiệm",
+            "Fix các lỗi về rank, lỗi mất đồ ngẫu nhiên",
+            "thêm hệ thống xếp hạng tier dựa trên chỉ số elo",
+            "nâng cấp từ paper 1.21.1 sang purpur 1.21.11, thêm vũ khí mới cây giáo và các địa hình, sinh vật ở 1.21.11",
+            "Cân bằng kinh tế: Tái cấu trúc nền kinh tế, tạo môi trường giao thương lành mạnh và cạnh tranh.",
+            "Ra mắt trang web chính của server TreMC và đồng bộ dữ liệu từ Server đến Web."
+        ]
+    },
+    {
+        "season": "1",
+        "date": "2026-05-17 - 2026-05-30",
+        "title": "fix lag, nâng cấp và từng bước hoàn hiện server Server",
+        "changes": [
+            "Fix các lỗi mà người chơi gặp phải",
+            "Thêm hệ thống fix lag và tối ưu server",
+            "Nâng diện tích các thế giới: World 5k --> 10k, Nether 3k --> 5k, End 2k -- 5k",
+            "Củng cố an ninh server với hệ thống Anti-Hack và bảo mật."
+        ]
+    },
+    {
+        "season": "1",
+        "date": "2026-05-16",
+        "title": "Big Update: Nâng cấp toàn diện",
+        "changes": [
+            "Đại tu toàn bộ hệ thống kinh tế (Economy) để cân bằng thị trường.",
+            "ra mắt vực lobby mới, các bản xếp hạng top player",
+            "Ra mắt hệ thống Rank, cơ chế PvP truyền thống và bộ lệnh cơ bản cho người chơi.",
+            "Chính thức ra mắt kênh Discord cộng đồng để kết nối người chơi.",
+            "Củng cố an ninh server với hệ thống Anti-Hack và bảo mật."
+        ]
+    },
+    {
+        "season": "1",
+        "date": "2026-05-14",
+        "title": "Tắt whitelist, mở cửa công khai",
+        "changes": [
+            "Mở cửa cho người chơi tham gia trải nghiệm.",
+            "Hoàn thiện khu vực Spawn cơ bản."
+        ]
+    },
+    {
+        "season": "1",
+        "date": "2026-05-12",
+        "title": "Update Economy và Dashboard",
+        "changes": [
+            "Thêm hệ thống kinh tế: /shop, /sell, /sellgui.",
+            "Tích hợp bảng thống kê dữ liệu người chơi trên màn hình."
+        ]
+    },
+    {
+        "season": "1",
+        "date": "2026-05-11",
+        "title": "Ngày thành lập TreMC",
+        "changes": [
+            "Khởi chạy trên nền tảng Paper 1.21.1.",
+            "Giai đoạn thử nghiệm đầu tiên (Vanilla Minecraft)."
+        ]
+    }
+];
